c3a76a175fe04ac4b1b315d8f41de5d53168be3b
nearbyz <ajanth988@gmail.com>
2025-07-01 20:08:01 +0400

    Create search-terms.js

---
+++ search-terms.js
@@ -0,0 +1,11 @@
+    // Prediction word sets
+window.presetWords = [
+  "apple", "apricot", "banana", "broccoli", "beetroot", "blueberry", "carrot", "cauliflower", "cabbage",
+  "dates", "dragon fruit", "durian", "eggplant", "fig", "grapes", "garlic", "guava", "jackfruit", "kiwi",
+  "lemon", "lettuce", "mango", "melon", "nectarine", "okra", "onion", "orange", "papaya", "peach", "pear",
+  "pineapple", "plum", "potato", "pumpkin", "radish", "raspberry", "spinach", "strawberry", "tomato",
+  "turnip", "watermelon", "zucchini",
+  "t-shirt", "jeans", "jacket", "saree", "shorts", "socks", "shoes", "watch", "belt", "hat", "scarf",
+  "burger", "pizza", "sandwich", "pasta", "biriyani", "shawarma", "noodles", "samosa", "salad", "grill",
+  "phone", "charger", "earphones", "laptop", "tablet", "camera", "power bank", "usb", "smartwatch", "tv"
+];
